5d7bc97d64ad4354a37dccc7861bc3b2b4bacf5c
Liam Costello <liam@scondoo.de>
2013-10-03 21:15:45 +0200

    Added default root for /game/*id*

--- app.js
+++ app.js
@@ -12,8 +12,15 @@ app.engine('html', hbs.__express);
 // Define the static public folder for css, images, etc.
 app.use(express.static('public'));
 
+// Begin listening to the port
 app.listen(port);
 
+// Set the path to root
 app.get('/', function(request, response) {
 	response.render('index');
+});
+
+app.get('/game/:id', function(request, response) {
+	game_id = request.params.id;
+	return response.send('You entered ' + game_id);
 });
