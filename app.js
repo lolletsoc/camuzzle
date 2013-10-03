42c04198e4af3beb16d038e7accb0f6972e211b7
Liam Costello <liam@scondoo.de>
2013-10-03 18:02:47 +0200

    Added stub/skeleton files for WebRTC hooks.

---
+++ app.js
@@ -0,0 +1,18 @@
+/**
+ * app.js
+ */
+var express = require('express');
+var app = express();
+
+var hbs = require('hbs');
+app.set('view engine', 'html');
+app.engine('html', hbs.__express);
+
+// Define the static public folder for css, images, etc.
+app.use(express.static('public'));
+
+app.listen(3000);
+
+app.get('/', function(request, response) {
+	response.render('index');
+});
